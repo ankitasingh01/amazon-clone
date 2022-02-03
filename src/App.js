4846b914b5b8478988fbc565389c8640dcfad097
ankitasingh01 <er.ankita9@gmail.com>
2022-02-03 16:50:35 +0530

    commit third

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          this is just to check if it is working
-          {/* I am feeling sleepy asf I dont know how to get rid of everyone here around me
-          this is just too sleepy and I have to give her money as well so that' what  it is
-          I wantto sleep for like 1 hour and then have lunch but /u was acting as if was studying
-          now I am going to turn this shit off and sleep finally */}
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //BEM
+    <div className="app">
+     <h1>Hello clever me lets hit it  :</h1>
+     {/* Header */}
+     {/* Home */}
     </div>
   );
 }
