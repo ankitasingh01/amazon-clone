76202414ea5218617afa09317956da2151c9de50
ankitasingh01 <er.ankita9@gmail.com>
2022-02-03 13:06:12 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          this is just to check if it is working
+          {/* I am feeling sleepy asf I dont know how to get rid of everyone here around me
+          this is just too sleepy and I have to give her money as well so that' what  it is
+          I wantto sleep for like 1 hour and then have lunch but /u was acting as if was studying
+          now I am going to turn this shit off and sleep finally */}
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBegczBx3lA_mKgOZapQ8jGmKt6FlU7w2U",
+    authDomain: "clone-3e4e9.firebaseapp.com",
+    projectId: "clone-3e4e9",
+    storageBucket: "clone-3e4e9.appspot.com",
+    messagingSenderId: "569090640209",
+    appId: "1:569090640209:web:72b44de857ac6af2da2b70",
+    measurementId: "G-B9CJZME5YJ"
+  };
