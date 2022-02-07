25f7bae7efebbf3c9c6fdf13faca2f3df3264258
ankitasingh01 <er.ankita9@gmail.com>
2022-02-07 07:07:20 +0530

    header first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     //BEM
     <div className="app">
-     <h1>Hello clever me lets hit it  :</h1>
+     <Header/>
      {/* Header */}
      {/* Home */}
     </div>
