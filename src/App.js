1132ef7ad4b8ce094c3b3f8128a6b1e7064f2e5b
ankitasingh01 <er.ankita9@gmail.com>
2022-02-10 04:38:54 +0530

    login beginning

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Home from './Home';
+import { BrowserRouter,Routes,Route,} from "react-router-dom";
+// Just Use Routes instead of "Switch"
+import Checkout from './Checkout';
 
 function App() {
   return (
     //BEM
+    <BrowserRouter>
     <div className="app">
      <Header/>
-     {/* Header */}
-     {/* Home */}
-    </div>
+      <Routes>
+        {/*  <Route path='/' 
+          element={<><Header/><Home/></>} />
+          this is to use two elements inside element */}
+          <Route path='/login' element={<h1>login page</h1>}/>
+          <Route path='/' element={<Home/>} />
+          <Route path='/checkout' element={<Checkout/>}/>
+
+
+      </Routes>
+         </div>
+    </BrowserRouter> 
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
-
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import {Link} from 'react-router-dom';
+import { useStateValue } from './StateProvider';
+// so you have to import link otherwise error
 function Header() {
+
+    const [{basket},dispatch] = useStateValue();
+
   return( 
   <div className='header'>
-
+      <Link to="/">
       <img
          className='header_logo'
          src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'
       />
+      </Link>
       <div className='header_search'>
           <input
             className='header_searchInput'
@@ -43,9 +50,12 @@ function Header() {
               className='header_optionLineTwo'>
                 Prime</span>
           </div>
-
-
-
+          <Link to='/checkout'>
+          <div className='header_optionBasket'>
+            <ShoppingBasketIcon/>
+            <span className='header_optionLineTwo header_basketCount'>{basket.length}</span>
+          </div>
+          </Link>
       </div>
 
   </div>
